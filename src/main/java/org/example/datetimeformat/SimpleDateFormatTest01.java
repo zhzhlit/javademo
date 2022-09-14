3818f706697840d50c172c95d7b086f7e1715109
张正亮 <zhangzhliang@yonyou.com>
2022-09-14 19:11:47 +0800

    feat(SimpleDateFormat): 线程不安全

---
+++ src/main/java/org/example/datetimeformat/SimpleDateFormatTest01.java
@@ -0,0 +1,57 @@
+package org.example.datetimeformat;
+
+import java.text.SimpleDateFormat;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+/**
+ * @author zhzhl
+ */
+public class SimpleDateFormatTest01 {
+
+    /**
+     * 执行总次数
+     */
+    private static final int EXECUTE_COUNT = 1000;
+    /**
+     * 同时运行的线程数量
+     */
+    private static final int THREAD_COUNT = 20;
+    /**
+     * SimpleDateFormat对象
+     */
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+    public static void main(String[] args) throws InterruptedException {
+        final Semaphore semaphore = new Semaphore(THREAD_COUNT);
+        final CountDownLatch countDownLatch = new CountDownLatch(EXECUTE_COUNT);
+
+        @SuppressWarnings("AlibabaThreadPoolCreation") ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for (int i = 0; i < EXECUTE_COUNT; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    try {
+                        SIMPLE_DATE_FORMAT.parse("2020-01-01");
+                    } catch (Exception e) {
+                        System.out.println("线程：" + Thread.currentThread().getName() + " 格式化日期失败");
+                        e.printStackTrace();
+                        System.exit(1);
+                    }
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    System.out.println("信号量发生错误");
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+                countDownLatch.countDown();
+            });
+        }
+        countDownLatch.await();
+        executorService.shutdown();
+        System.out.println("所有线程格式化日期成功");
+    }
+}
